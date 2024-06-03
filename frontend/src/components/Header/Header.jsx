7a8e6e522f011203999a7d344519556afedd6d94
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-06-03 18:48:51 +0530

    show the Food and using useContext and show food list accoding to their selected items and create demo for add the food items to the cart

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favurite food here.</h2>
+        <p>
+          Experience the classic taste of Italy with our Margherita Pizza. Fresh
+          mozzarella cheese, ripe tomatoes, and aromatic basil leaves are
+          layered on a crispy, wood-fired crust. Drizzled with extra virgin
+          olive oil, this pizza is a perfect blend of simplicity and flavor
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
