cfb40b80197b5534678568e1100bdda90003cf66
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-06-04 07:16:37 +0530

    add loginPopup ui

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../../assets/assets";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
 
   return (
@@ -46,7 +46,10 @@ const Navbar = () => {
           <div className="dot"></div>
         </div>
         <div className="navbar-div-button">
-          <button className="navbar-button"> sign-in</button>
+          <button onClick={() => setShowLogin(true)} className="navbar-button">
+            {" "}
+            sign-in
+          </button>
         </div>
       </div>
     </div>
