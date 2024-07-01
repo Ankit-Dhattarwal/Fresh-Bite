7babff00c4ca81cbbe488f2670852d29bd083b22
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-07-02 02:06:41 +0530

    deploy our website on render

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItem, setCartItem] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://fresh-bite-backend.onrender.com";
   const [token, setToken] = useState(localStorage.getItem("token") || "");
 
   const [food_list, setFoodList] = useState([]);
